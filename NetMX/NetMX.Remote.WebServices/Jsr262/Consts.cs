b1e670cfc388fed41b48182b7db26ea0ebca1b31
SzymonPobiega_cp <7e1df2fc7b7eb1de3ab65aeecb7227e75e7ca2bfAUJZqsgx>
2010-01-03 13:11:13 +0000

    Re-packaging...
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS04]$/NetMX;C53470

--- NetMX/NetMX.Remote.WebServices/Jsr262/Consts.cs
+++
@@ -1,7 +0,0 @@
-namespace NetMX.Remote.WebServices.Jsr262
-{
-    internal static class Consts
-    {
-        internal const string DynamicMBeanResourceUri = "http://jsr262.dev.java.net/DynamicMBeanResource";
-    }
-}
--- NetMX/NetMX.Remote.WebServices/WsAddressing/WsAddr.cs
+++
@@ -1,7 +0,0 @@
-namespace NetMX.Remote.WebServices.WsAddressing
-{
-    public static class Consts
-    {
-        public const string WsAddressingNamespace = "http://schemas.xmlsoap.org/ws/2004/08/addressing";
-    }
-}
