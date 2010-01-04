13c83c8fc2499d2353236c28e8441c177434af87
SzymonPobiega_cp <7e1df2fc7b7eb1de3ab65aeecb7227e75e7ca2bfAUJZqsgx>
2010-01-04 20:10:29 +0000

    New JSR-262 demo for v 0.7
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS04]$/NetMX;C53563

---
+++ NetMX/NetMX.Remote.Jsr262/Server/ServerAddressExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace NetMX.Remote.Jsr262
+{
+   public sealed class ServerAddressExtension : IExtension<OperationContext>
+   {
+      private readonly Uri _address;
+
+      public ServerAddressExtension(Uri address)
+      {
+         _address = address;
+      }
+
+      public Uri Address
+      {
+         get { return _address; }
+      }
+
+      public void Attach(OperationContext owner)
+      {         
+      }
+
+      public void Detach(OperationContext owner)
+      {         
+      }
+   }
+}
---
+++ NetMX/Samples/Jsr262Demo/Program.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NetMX;
+using NetMX.Remote;
+using System.Security.Principal;
+
+namespace Jsr262Demo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			AppDomain.CurrentDomain.SetPrincipalPolicy(PrincipalPolicy.WindowsPrincipal);
+			IMBeanServer server = MBeanServerFactory.CreateMBeanServer();
+			Sample o = new Sample();
+			ObjectName name = new ObjectName("Sample:a=b");
+			server.RegisterMBean(o, name);
+         Uri serviceUrl = new Uri("http://localhost/MBeanServer");            
+
+			using (INetMXConnectorServer connectorServer = NetMXConnectorServerFactory.NewNetMXConnectorServer(serviceUrl, server))
+			{
+				connectorServer.Start();
+
+				using (INetMXConnector connector = NetMXConnectorFactory.Connect(serviceUrl, null))
+				{
+					IMBeanServerConnection remoteServer = connector.MBeanServerConnection;
+
+               //Not supported yet in JSR-262 connector.
+               //remoteServer.AddNotificationListener(name, CounterChanged, null, null);
+
+               Console.WriteLine("******");
+               MBeanInfo info = remoteServer.GetMBeanInfo(name);
+               Console.WriteLine("MBean description: {0}", info.Description);
+               Console.WriteLine("MBean class name: {0}", info.ClassName);
+               foreach (MBeanAttributeInfo attributeInfo in info.Attributes)
+               {
+                  Console.WriteLine("Attribute {0} ({1}) [{2}{3}]: {4}", attributeInfo.Name, attributeInfo.Description,
+                     attributeInfo.Readable ? "r" : "", attributeInfo.Writable ? "w" : "", attributeInfo.Type);
+               }
+               foreach (MBeanOperationInfo operationInfo in info.Operations)
+               {
+                  Console.WriteLine("Operation {0} ({1}) [{2}]", operationInfo.Name, operationInfo.Description,
+                    operationInfo.Impact);
+               }
+               Console.WriteLine("******");
+
+               object counter = remoteServer.GetAttribute(name, "Counter");
+
+               Console.WriteLine("Counter value is {0}", counter);
+
+               remoteServer.SetAttribute(name, "Counter", 5);
+               counter = remoteServer.GetAttribute(name, "Counter");
+
+               Console.WriteLine("Now, counter value is {0}", counter);
+
+               counter = remoteServer.Invoke(name, "AddAmount", new object[] { 5 });
+               counter = remoteServer.GetAttribute(name, "Counter");
+
+               Console.WriteLine("Now, counter value is {0}", counter);
+
+               counter = remoteServer.Invoke(name, "ResetCounter", new object[] { });
+               counter = remoteServer.GetAttribute(name, "Counter");
+
+               Console.WriteLine("Now, counter value is {0}", counter);
+
+					Console.ReadKey();
+				}
+			}
+		}
+		static void CounterChanged(Notification notification, object handback)
+		{
+			Console.WriteLine("Counter changed! New value is {0}", notification.UserData);
+		}
+	}
+}
