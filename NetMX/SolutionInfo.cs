7658bf13c7c0eefd8a210e9df5f1e80e616823e4
SzymonPobiega_cp <7e1df2fc7b7eb1de3ab65aeecb7227e75e7ca2bfAUJZqsgx>
2010-01-13 15:55:00 +0000

    0.71 / 0.7.0.1 - structure refactoring
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS04]$/NetMX;C54208

---
+++ NetMX/SolutionInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NetMX")]
+[assembly: AssemblyCopyright("Copyright ©  2008-2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0d2730d2-762c-4c6d-8998-9f0713cc91b2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("0.7.0.1")]
+[assembly: AssemblyFileVersion("0.7.0.1")]
+[assembly: System.CLSCompliant(true)]
