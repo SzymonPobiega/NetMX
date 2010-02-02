9d9d570f97f99821a8ce29777062957ef795ed84
SzymonPobiega_cp <7e1df2fc7b7eb1de3ab65aeecb7227e75e7ca2bfAUJZqsgx>
2010-02-02 04:27:49 +0000

    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS04]$/NetMX;C55182

--- NetMX/SolutionInfo.cs
+++ NetMX/SolutionInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.7.0.1")]
-[assembly: AssemblyFileVersion("0.7.0.1")]
+[assembly: AssemblyVersion("0.7.0.0")]
+[assembly: AssemblyFileVersion("0.7.0.0")]
 [assembly: System.CLSCompliant(true)]
