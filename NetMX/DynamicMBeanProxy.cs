05f79fb12c91762710238ed464d29a303664ef30
Szymon Pobiega <szymon.pobiega@gmail.com>
2012-03-04 07:47:06 +0100

    Added comments to dynamic proxy

--- NetMX/DynamicMBeanProxy.cs
+++ NetMX/DynamicMBeanProxy.cs
@@ -3,29 +3,56 @@ using System.Dynamic;
 
 namespace NetMX
 {
+    ///<summary>
+    /// A dynamic object that proxies a remote MBean.
+    ///</summary>
     public class DynamicMBeanProxy : DynamicObject
     {
         private readonly ObjectName _objectName;
         private readonly IMBeanServerConnection _serverConnection;
 
+        ///<summary>
+        /// Creates new instance of <see cref="DynamicMBeanProxy"/>.
+        ///</summary>
+        ///<param name="objectName"></param>
+        ///<param name="serverConnection"></param>
         public DynamicMBeanProxy(ObjectName objectName, IMBeanServerConnection serverConnection)
         {
             _objectName = objectName;
             _serverConnection = serverConnection;
         }
 
+        ///<summary>
+        /// Forwards method invocation as operation invocation to remote server via a connection.
+        ///</summary>
+        ///<param name="binder"></param>
+        ///<param name="args"></param>
+        ///<param name="result"></param>
+        ///<returns></returns>
         public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
         {
             result = _serverConnection.Invoke(_objectName, binder.Name, args);
             return true;
         }
 
+        ///<summary>
+        /// Forwards property get action as attribute value retrieval to remote server via a connection.
+        ///</summary>
+        ///<param name="binder"></param>
+        ///<param name="result"></param>
+        ///<returns></returns>
         public override bool TryGetMember(GetMemberBinder binder, out object result)
         {
             result = _serverConnection.GetAttribute(_objectName, binder.Name);
             return true;
         }
 
+        /// <summary>
+        /// Forwards property set action as attribute value update to remote server via a connection.
+        /// </summary>
+        /// <param name="binder"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public override bool TrySetMember(SetMemberBinder binder, object value)
         {
             _serverConnection.SetAttribute(_objectName, binder.Name, value);
--- NetMX/MBeanServerConnectionExtensions.cs
+++ NetMX/MBeanServerConnectionExtensions.cs
@@ -1,7 +1,16 @@
 namespace NetMX
 {
+    ///<summary>
+    /// Extension methods for <see cref="IMBeanServerConnection"/>.
+    ///</summary>
     public static class MBeanServerConnectionExtensions
     {        
+        ///<summary>
+        /// Creates a new dynamic proxy object that forwards method invocations and property gets/sets to specified <see cref="IMBeanServerConnection"/>.
+        ///</summary>
+        ///<param name="connection">A connection to use.</param>
+        ///<param name="name">An object name of MBean that this proxy applies to.</param>
+        ///<returns></returns>
         public static dynamic CreateDynamicProxy(this IMBeanServerConnection connection, ObjectName name)
         {
             return new DynamicMBeanProxy(name, connection);
